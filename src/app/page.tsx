802d4114719d068984b1d636ed9866524ca32266
malak1e <david@productifylabs.io>
2024-02-15 18:33:16 +0200

    feat: removed sliding bar and added the bubblesort algorithm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,55 @@
 "use client";
-import ArraySizeSlider from "@/components/ArraySizeSlider";
 import SortingLine from "@/components/SortingLine";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [numberOfSortingLines, setNumberOfSortingLines] = useState<number>(50);
-  const [sortingLines, setSortingLines] = useState<JSX.Element[]>([]);
+  const [numberOfSortingLines, setNumberOfSortingLines] = useState<number[]>(
+    Array.from({ length: 240 }, (_, index) => index + 1)
+  );
 
-  useEffect(() => {
-    setSortingLines(
-      Array.from({ length: numberOfSortingLines }, (_, index) => (
-        <SortingLine
-          key={index}
-          totalLines={numberOfSortingLines}
-          height={index * 3}
-        />
-      ))
-    );
-  }, [numberOfSortingLines]);
+  const shuffleLines = () => {
+    const array = Array.from({ length: 240 }, (_, index) => index + 1);
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    setNumberOfSortingLines(array);
+  };
+
+  const bubbleSort = async () => {
+    let linesCopy = [...numberOfSortingLines];
+    let temp;
+    for (let i = 0; i < linesCopy.length; i++) {
+      for (let j = 0; j < linesCopy.length - i - 1; j++) {
+        if (linesCopy[j] > linesCopy[j + 1]) {
+          temp = linesCopy[j];
+          linesCopy[j] = linesCopy[j + 1];
+          linesCopy[j + 1] = temp;
+          setNumberOfSortingLines([...linesCopy]);
+          await new Promise((resolve) => setTimeout(resolve, 1));
+        }
+      }
+    }
+  };
 
   return (
     <>
-      <nav className="p-2">
-        <ArraySizeSlider
-          arraySize={numberOfSortingLines}
-          onChange={(e) => setNumberOfSortingLines(e.target.value)}
-        />
+      <nav className="p-2 flex justify-around cursor-pointer">
+        <div className="font-semibold text-sm p-2" onClick={shuffleLines}>
+          Randomize Lines
+        </div>
+        <button onClick={() => bubbleSort()}>Bubble Sort</button>
       </nav>
       <div className="flex justify-center items-center">
-        <div className="w-1/2 flex justify-center">{sortingLines}</div>
+        <div className="w-1/2 flex justify-center">
+          {numberOfSortingLines.map((num, idx) => (
+            <SortingLine
+              height={(num + 1) * 3}
+              totalLines={numberOfSortingLines.length}
+              key={Math.random()}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
--- src/components/ArraySizeSlider/index.tsx
+++ src/components/ArraySizeSlider/index.tsx
@@ -3,20 +3,25 @@ import React from "react";
 type ArraySizeSlider = {
   arraySize: number;
   onChange: (e: any) => void;
+  numberOfLines: number;
 };
 
-const ArraySizeSlider = ({ arraySize, onChange }: ArraySizeSlider) => {
+const ArraySizeSlider = ({
+  arraySize,
+  onChange,
+  numberOfLines,
+}: ArraySizeSlider) => {
   return (
     <div>
       <label htmlFor="arraySize" className="font-semibold text-sm mr-2">
-        Change Array Size:{" "}
+        # of lines {numberOfLines}
       </label>
       <input
         className="h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
         type="range"
         id="arraySize"
         min="50"
-        max="200"
+        max="240"
         step="1"
         value={arraySize}
         onChange={onChange}
